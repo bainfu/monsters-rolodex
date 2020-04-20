ebe56ccfe4e841afdb104f56674b5123837eeb6c
Bain Kennedy <bain.kennedy@mobileforming.com>
2020-04-19 17:33:05 -0700

    Updates

---
+++ precache-manifest.975d07488f2dc40e84d093e1286f94c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33fa4308965f82c1f97f474144bbfa2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ba3a2b08af616d000230",
+    "url": "/monster-rolodex/static/css/main.8e7e78c8.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3a2b08af616d000230",
+    "url": "/monster-rolodex/static/js/main.ac2fba66.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
